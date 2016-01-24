39ccf8a8b3dbe4122d18f281c8e9261dabfee207
Thiago Giuliano Calistro <tgcalistro@gmail.com>
2016-01-24 19:11:03 -0200

    Atualizando versao
    Atualizando

---
+++ testando.php
@@ -0,0 +1,4 @@
+<?php 
+	$conexao = mysql_connect("mysql09.chiptronic1.hospedagemdesites.ws", "chiptronic18", "jew*349") or die(mysql_error()); 
+	$db = mysql_select_db("chiptronic18", $conexao);
+?>
---
+++ valores.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+
+<body>
+<?php 
+	$conexao = mysql_connect("mysql09.chiptronic1.hospedagemdesites.ws", "chiptronic18", "jew*349") or die(mysql_error()); 
+	$db = mysql_select_db("chiptronic18", $conexao);
+?>
+<?php
+ $id_visitante=$_GET["cod_visitante"];
+ $qtd2=$_GET["qtd2"];
+ $date = $_GET['ano'];
+ $sql = mysql_query('select * from tb_visitantes where cod_visitante='.$id_visitante.' and data='.$date.'');
+ while($ln = mysql_fetch_array($sql)){
+ $mes = $ln['mes_visitante'];
+ $primeiras_letras_2 = substr($mes,0,3);
+ $qtd = $ln['quantidade_visitante'];
+  echo $primeiras_letras_2.'. ', number_format($qtd, 0, '', '.')." visitas";
+ $totalcomp = ($qtd - $qtd2)/100;
+ 
+ if($qtd < $qtd2){
+  echo "<span class='decresceu'> ".$totalcomp."%</span>";
+ }else{
+  echo "<span class='cresceu'> ".$totalcomp."%</span>";
+ }
+ }
+?>
+</body>
+</html>
